648686e15e72d7353442b0f8a28054cc3bcbcebf
Chase Squires <ccsquires@smu.edu>
2015-08-31 20:32:09 -0500

    beginning structure

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!" << endl;
+    if(argc != 6){
+        cout <<"Usage: " <<argv[0] << " <FirstTeamFile> <SecondTeamFile> <MatchFile> <OutputFileName> <verbosity>"<<endl;
+    }
+    else{
+        //continue program
+        ifstream file_matchData ("match1.txt");
+
+        if(file_matchData.is_open()){
+            //could not be opened
+        } else {            file_matchData.rdbuf();
+        }
+    }
     return 0;
 }
 
